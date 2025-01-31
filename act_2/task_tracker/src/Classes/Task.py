fb4e2f978aa0b1c920609214a977faff798cee30
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2025-01-31 00:02:47 -0400

    Comentando

--- act_2/task_tracker/src/Classes/Task.py
+++ act_2/task_tracker/src/Classes/Task.py
@@ -2,8 +2,11 @@ import typer
 import json
 from prettytable import PrettyTable
 
+# Crear el inicializador del Typer
 app = typer.Typer()
 
+
+# Crear tarea y usar el comando para ser reconocido por el Typer
 @app.command()
 def crear_tarea(name: str, description: str):
     print(f"Task created: {name} - {description}")
@@ -18,7 +21,8 @@ def crear_tarea(name: str, description: str):
 
     with open("Data/Data.json", "w") as file:
         json.dump(data, file)
-        
+
+    # Listado de Tareas
     ver_tareas()
         
 
@@ -27,7 +31,9 @@ def ver_tareas():
     with open("Data/Data.json", "r") as file:
         data = json.load(file)
         table = PrettyTable()
+        # Asignacion de los encabezados de la tabla
         table.field_names = ["Name", "Description"]
         for task in data:
+            #Agregar filas a la tabla
             table.add_row([task["name"], task["description"]])
         print(table)
